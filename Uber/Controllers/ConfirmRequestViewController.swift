6083d23d08fe0404fcae96d79c1652f36d9a903f
Glauber Gustavo <gustavo.glauber@gmail.com>
2023-01-14 03:24:47 -0300

    first commit

---
+++ Uber/Controllers/ConfirmRequestViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ConfirmRequestViewController.swift
+//  Uber
+//
+//  Created by Glauber Gustavo on 23/08/21.
+//
+
+import UIKit
+
+class ConfirmRequestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Uber/Controllers/PassengerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PassengerViewController.swift
+//  Uber
+//
+//  Created by Glauber Gustavo on 22/08/21.
+//
+
+import UIKit
+
+class PassengerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
